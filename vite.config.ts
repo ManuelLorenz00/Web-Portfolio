7ab633689cf73e34fbf177ab891fb0c723bb72e6
ManuelLorenz00 <107814082+ManuelLorenz00@users.noreply.github.com>
2023-05-23 20:08:46 +0200

    changed ts config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-manuellorenz/',
+  base: '/Web-Portfolio/',
   plugins: [vue()],
   resolve: {
     alias: {
